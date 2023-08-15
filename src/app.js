854c69f7615049246663e1824ce5ed4fc0939645
JJMorales <jjmorales.dev@gmail.com>
2023-08-15 18:04:23 -0500

    feat: :sparkles: cree el modulo que ejecuta el servidor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 
+import express from "express";
+import morgan from "morgan";
+import { router } from "./routes/index.js";
+const app = express();
 
+// midlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
-
-
+// rutas
+app.use(router);
+export default app;
