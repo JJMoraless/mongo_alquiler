ecf9dd965bbd56939c018250f4f84587062a9b9e
JJMoraless <jjmorales.dev@gmail.com>
2023-08-16 14:28:37 -0500

    feat: :sparkles: agregado endpoint de automoviles dispobles para alquiler

--- src/routes/automoviles.routes.js
+++ src/routes/automoviles.routes.js
@@ -3,11 +3,46 @@ import db from "../db/config.js";
 const automovil = db.collection("automovil");
 const router = Router();
 
-// 15. Listar todos los automóviles ordenados por marca y modelo.
+// 16. Listar todos los automóviles ordenados por marca y modelo.
 router.get("/", async (req, res) => {
   try {
     const autosFound = await automovil.find().sort({ Marca: 1 }).toArray();
     res.json({ status: 200, automoviles: autosFound });
+  } catch (error) {
+    res.status(500)({ status: 500, error });
+  }
+});
+
+// 3. Obtener todos los automóviles disponibles para alquiler.
+router.get("/disponibles", async (req, res) => {
+  try {
+    const autosDispo = await alquiler
+      .aggregate([
+        {
+          $lookup: {
+            from: "automovil",
+            localField: "ID_Automovil",
+            foreignField: "ID_Automovil",
+            as: "automovil",
+          },
+        },
+        {
+          $unwind: "$automovil",
+        },
+        {
+          $match: {
+            Estado: "Disponible",
+          },
+        },
+        {
+          $project: {
+            Estado: 1,
+            automovil: 1,
+          },
+        },
+      ])
+      .toArray();
+    res.json({ status: 200, automoviles_disponibles: autosDispo });
   } catch (error) {
     res.status(500)({ status: 500, error });
   }
