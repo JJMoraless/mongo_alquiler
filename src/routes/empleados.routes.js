7839a3b1a791f72da43bdd500ac5a034525927a1
JJMoraless <jjmorales.dev@gmail.com>
2023-08-16 11:09:55 -0500

    feat: :sparkles: añadido endpoint de listar empleados gerente y asistente

--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -3,8 +3,7 @@ import db from "../db/config.js";
 const router = Router();
 const empleado = db.collection("empleado");
 
-
-// 7. Listar los empleados con el cargo de "Vendedor". 
+// 7. Listar los empleados con el cargo de "Vendedor".
 router.get("/vendedores", async (req, res) => {
   try {
     const empeladosFound = await empleado.find({ Cargo: "Vendedor" }).toArray();
@@ -14,5 +13,16 @@ router.get("/vendedores", async (req, res) => {
   }
 });
 
+// 13. Mostrar los empleados con cargo de "Gerente" o "Asistente".
+router.get("/gerente_asistente", async (req, res) => {
+  try {
+    const empeladosFound = await empleado
+      .find({ $or: [{ Cargo: "Gerente" }, { Cargo: "Asistente" }] })
+      .toArray();
+    res.json({ status: 200, empleados_gerenteasistente: empeladosFound });
+  } catch (error) {
+    res.status(500)({ status: 500, error });
+  }
+});
 
 export { router };
