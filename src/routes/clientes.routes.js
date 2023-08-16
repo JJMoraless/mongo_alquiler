aa3888941d957688acd988fa3ae988d26089adbf
JJMoraless <jjmorales.dev@gmail.com>
2023-08-16 10:54:06 -0500

    feat: :sparkles: añdido endpoint de traer los clientes con al menos un alquiler

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -3,6 +3,7 @@ import db from "../db/config.js";
 
 const router = Router();
 const clientes = db.collection("cliente");
+const alquiler = db.collection("alquiler");
 
 
 // 2. Mostrar todos los clientes registrados en la base de datos.
@@ -11,6 +12,35 @@ router.get("/", async (req, res) => {
   try {
     const clientesFound = await clientes.find().toArray();
     res.json({ status: 200, clientes: clientesFound });
+  } catch (error) {
+    res.status(500)({ status: 500, error });
+  }
+});
+
+router.get("/unalquiler", async (req, res) => {
+  try {
+    const reservasFound = await alquiler
+      .aggregate([
+        {
+          $lookup: {
+            from: "cliente",
+            localField: "ID_Cliente",
+            foreignField: "ID_Cliente",
+            as: "cliente",
+          },
+        },
+        {
+          $unwind: "$cliente",
+        },
+        {
+          $project: {
+            cliente: 1,
+            _id: 0,
+          },
+        },
+      ])
+      .toArray();
+    res.json({ status: 200, clientes_reserva: reservasFound });
   } catch (error) {
     res.status(500)({ status: 500, error });
   }
