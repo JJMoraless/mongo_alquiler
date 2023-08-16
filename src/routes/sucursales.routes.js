abf268cc7f95c7aea3c719692d3e8f5b221f79c3
JJMoraless <jjmorales.dev@gmail.com>
2023-08-16 14:20:20 -0500

    feat: :sparkles: endpoint de mostrar la cantidad total de automoviles + la direccion

--- src/routes/sucursales.routes.js
+++ src/routes/sucursales.routes.js
@@ -33,4 +33,41 @@ router.get("/autos_totales", async (req, res) => {
   }
 });
 
+// 16. Mostrar la cantidad total de automóviles en cada sucursal junto con su dirección.
+router.get("/autos_totales_direccion", async (req, res) => {
+  try {
+    const sucursalesAutoFound = await sucursalAuto
+      .aggregate([
+        {
+          $lookup: {
+            from: "sucursal",
+            localField: "ID_Sucursal",
+            foreignField: "ID_Sucursal",
+            as: "sucursal",
+          },
+        },
+        {
+          $unwind: "$sucursal",
+        },
+        {
+          $group: {
+            _id: "$sucursal.Direccion",
+            totalCantidad: { $sum: "$Cantidad_Disponible" },
+          },
+        },
+        {
+          $project: {
+            direccion: "$_id", // Renombrar campo _id
+            totalCantidad: 1, // Mantener totalCantidad
+            _id: 0,
+          },
+        },
+      ])
+      .toArray();
+    res.json({ status: 200, autos_sucursales: sucursalesAutoFound });
+  } catch (error) {
+    res.status(500).json({ status: 500, error });
+  }
+});
+
 export { router };
