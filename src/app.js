6f88a3c8a6d9fb530b793deab7490162f949627f
JJMoraless <jjmorales.dev@gmail.com>
2023-08-16 18:06:15 -0500

    feat: :sparkles: agregado modulo de autenticacion con jwt

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 import express from "express";
 import morgan from "morgan";
 import { router } from "./routes/index.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 // midlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 // rutas
 app.use(router);
